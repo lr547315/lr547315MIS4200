b47dcaabc8500e0d048338a3ec9ac329051e7c8f
Leticia Rodrigues <lr547315@ohio.edu>
2020-01-27 11:41:31 -0500

    IC 01/27 - Customer Class

---
+++ Models/customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace lr547315MIS4200.Models
+{
+    public class customer
+    {
+        public int customerId { get; set; }
+        public string customerFirstName { get; set; }
+
+        public string customerLastName { get; set; }
+
+        public string email { get; set; }
+
+        public string phone { get; set; }
+        public DateTime customerSince { get; set; }
+    }
+}
